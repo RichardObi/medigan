07a04d25411b72f3b17f486cadbbfc59d3cb80b2
RichardObi <Richard.Osuala@gmail.com>
2021-08-12 19:34:40 +0200

    Now added tests folder and updated setup.py and .gitignore.

--- setup.py
+++ setup.py
@@ -5,19 +5,23 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="medigan",                  
-    version="3.2",                        
+    version="0.0.1",
     author="Noussair Lazrak, Richard Osuala, Kaisar Kushibar, Oliver Díaz, Karim Lekadir",                     
-    description="MediGAN is a Python library to implement Generative Adversarial Networks(GANs), Conditional GANs, Adversarial Auto-Encoders(AAEs) etc. This library aims to enhance data augmentation via providing/ generating training data sets for other Deep learning models.",
+    description="MediGAN is a Python library to implement Generative Adversarial Networks(GANs), Conditional GANs, Adversarial Auto-Encoders(AAEs), etc. This library aims to enhance data augmentation via providing/ generating training data sets for other Deep learning models.",
     long_description=long_description,      
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    
+    packages=setuptools.find_packages(),
+    url="https://github.com/RichardObi/GANtoolbox",
+    project_urls={
+        "Bug Tracker": "https://github.com/RichardObi/GANtoolbox/issues",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],                                      
-    python_requires='>=2.6',                
-    py_modules=["medigan"],             
-    package_dir={'':'medigan'},     
+    python_requires='>=3.6',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     install_requires=["Path","Union","pyyaml","numpy","torch","opencv-contrib-python-headless"]
 )
