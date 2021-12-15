f0b7b0b45ac46797e6c3a59ddc36fa070bd33e26
RichardObi <Richard.Osuala@gmail.com>
2021-12-15 19:35:02 +0100

    Added script for quick model integration test in tests/model_integration_test.py

---
+++ tests/model_integration_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# ! /usr/bin/env python
+""" script for quick local testing if new models work inside medigan.
+
+.. codeauthor:: Richard Osuala <richard.osuala@gmail.com>
+"""
+# run with python -m tests.model_integration_test
+
+import glob
+import logging
+import shutil
+import sys
+import unittest
+
+MODEL_ID = "YOUR_MODEL_ID_HERE"
+NUM_SAMPLES = 10
+OUTPUT_PATH= f"output/{MODEL_ID}/"
+try:
+    from src.medigan.generators import Generators
+    generators = Generators()
+except Exception as e:
+    logging.error(f"test_init_generators error: {e}")
+    raise e
+
+generators.generate(model_id=MODEL_ID, num_samples=NUM_SAMPLES, output_path=OUTPUT_PATH)
