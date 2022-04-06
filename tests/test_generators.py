5d114d30f4c92e61d973b1b99d2b57496540ce26
Grzegorz Skorupko <grzegorz.skorupko@gmail.com>
2022-04-06 15:45:07 +0200

    Add generator tests

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -4,8 +4,7 @@ from medigan import Generators
 def test_generate():
     generators = Generators()
 
-    # images = generators.generate(model_id="00001_DCGAN_MMG_CALC_ROI", num_samples=10, save_images=False)
-    images = generators.generate(model_id="00001_DCGAN_MMG_CALC_ROI", num_samples=10)
+    images = generators.generate(model_id="00001_DCGAN_MMG_CALC_ROI", num_samples=10, save_images=False)
 
     assert len(images) == 10
 
