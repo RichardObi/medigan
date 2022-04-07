05ac68c9afcd735835b8c652ac4203b8c8226458
Grzegorz Skorupko <grzegorz.skorupko@gmail.com>
2022-04-07 09:51:37 +0200

    Add parametrized tests of models

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,10 +1,15 @@
+import pytest
 from medigan import Generators
 
+models_list = ["00001_DCGAN_MMG_CALC_ROI", 
+"00002_DCGAN_MMG_MASS_ROI",
+"00003_CYCLEGAN_MMG_DENSITY_FULL"
+]
 
-def test_generate():
+@pytest.mark.parametrize("model_id", models_list)
+def test_generate_model(model_id):
     generators = Generators()
 
-    images = generators.generate(model_id="00001_DCGAN_MMG_CALC_ROI", num_samples=10, save_images=False)
+    images = generators.generate(model_id=model_id, num_samples=3, save_images=False)
 
-    assert len(images) == 10
-
+    assert len(images) == 3
