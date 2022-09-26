be656dcf92579224406192e700083109bb24cfa2
RichardObi <Richard.Osuala@gmail.com>
2022-09-26 16:01:13 +0200

    Preparation of windows test of model 11 and 21

--- tests/test_model_executor.py
+++ tests/test_model_executor.py
@@ -127,9 +127,9 @@ class TestMediganExecutorMethods:
 
         self.logger.debug(f"models: {models_with_args}")
         for i, model_id in enumerate(self.model_ids):
-            # if model_id == '00011_SINGAN_POLYP_PATCHES_W_MASKS':
+            if model_id != '00011_SINGAN_POLYP_PATCHES_W_MASKS' and model_id != "00021_CYCLEGAN_BRAIN_MRI_T1_T2":
             ## avoiding full memory on Windows ci test server
-            # continue
+                continue
             self.logger.debug(f"Now testing model {model_id}")
             self._remove_dir_and_contents()  # Already done in each test independently, but to be sure, here again.
             self.test_generate_method(model_id=model_id)
